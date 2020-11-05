a82625e9ae3f204df8061950d1b55356163c4481
xiaodai <darope@163.com>
2020-11-06 00:03:31 +0800

    http请求参数结构体绑定&文件上传下载

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module easy-go-web
 
 go 1.15
 
-require (
-	github.com/gin-gonic/gin v1.6.3
-)
+require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"path"
 )
 
 func main() {
@@ -108,6 +109,71 @@ func main() {
 			"Password": password,
 		})
 
+	})
+
+	// 接口6 获取路径参数 127.0.0.1:8000/get-name/xiaodai/18
+	engine.GET("/get-name/:username/:age", func(c *gin.Context) {
+
+		// 获取路径参数
+		username := c.Param("username")
+		age := c.Param("age")
+
+		data := gin.H{"name": username, "age": age}
+
+		// 返回状态码
+		c.JSON(http.StatusOK, data)
+
+	})
+
+	// 接口7 请求参数和自定义结构体进行绑定。POST 127.0.0.1:8000/login-binding {"username":"xiaodai", "password":"12345"}
+	type UserInfo struct {
+		Username string `json:"username"` // 首字母大写表示可以导出。tag表示导出后显示小写
+		Password string
+	}
+	engine.POST("/login-binding", func(c *gin.Context) {
+		var u UserInfo // 声明一个userInfo类型的结构体类型变量
+		// 把请求参数绑定到我们的u类型上，注意，这里是为了修改u，所以不能传递值，需要传递引用。
+		// 且变量首字母需要大写，小写表示不可导出。返回不到前端
+		// c.ShouldBind(&u)不仅可以绑定post的body的参数，也可以绑定get请求的请求头参数:?username=a&password=12345。也可以绑定json的参数。按照字段名称进行绑定
+		err := c.ShouldBind(&u)
+		if err != nil { // 绑定失败则返回502错误信息
+			c.JSON(http.StatusBadGateway, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			// 返回状态码
+			c.JSON(http.StatusOK, u)
+		}
+	})
+
+	// 接口7 上传文件
+	engine.LoadHTMLFiles("./upload.html")
+	engine.GET("/file-page", func(c *gin.Context) {
+		// 返回状态码
+		c.HTML(http.StatusOK, "upload.html", nil)
+
+	})
+
+	engine.POST("/upload", func(c *gin.Context) {
+		// 通过文件名从请求中接受文件，将接受到的文件保存在服务器本地
+		file, err := c.FormFile("file")
+
+		// 多文件上传。保存到files，循环files逐个保存
+		//form, _ := c.MultipartForm()
+		//files := form.File["file"]
+
+		if err != nil { // 接受失败，返回错误
+			c.JSON(http.StatusBadGateway, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			// 将文件保存到本地
+			p := path.Join("./%s", file.Filename)
+			_ = c.SaveUploadedFile(file, p) // 可以通过设置上传文件缓冲区大小，来增加上传速度: engine.MaxMultipartMemory = 8 << 20
+			c.JSON(http.StatusOK, gin.H{
+				"ok": "OK",
+			})
+		}
 	})
 
 	engine.Run(":8000")
